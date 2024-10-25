02df8e02c3ea4bde3f58b6bb037614b596927972
HansonYin <jj.yin@qq.com>
2024-10-25 15:23:24 +0800

    Update git_test1.py with correct git_repo

--- git_test1.py
+++ git_test1.py
@@ -1,4 +1,4 @@
 print("hello github")
 print("branch a") 
 print("branch b")
-#git remote add origin https://github.com/Under-the-dome/Test.git
+#git remote add origin https://github.com/Under-the-dome/lm_select_git_test.git
